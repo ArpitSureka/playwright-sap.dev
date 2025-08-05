9a4dc10c4e9a9e358412b7e316b0ca9a10884f67
Arpit Sureka - a0s16ic <Arpit.Sureka@walmart.com>
2025-08-05 20:50:16 +0530

    Deploy website - based on 6078d8c44005f6aa50edbd6d4c9380f1a56dfbec

--- README.md
+++
@@ -1,4 +0,0 @@
-# Playwright-SAP 
-
-This repo contains the documentation/issues/feature-request for Playwright-SAP.
-Note: This repo does not contains actual code for Playwright-SAP module.
---
+++ assets/js/a7456010.d2bec331.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkplaywright_sap_docs=self.webpackChunkplaywright_sap_docs||[]).push([[235],{8552:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
